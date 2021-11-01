28228a0d6c2e6d3674278f9d6646496d5b8860ae
9071-dynamix <88184928+9071-dynamix@users.noreply.github.com>
2021-11-01 20:30:52 +0900

    create ``FaStrat``
    .hppにFireFly_Algorithm_Strategyの作成

--- lib/fa/fa_default.hpp
+++ lib/fa/fa_default.hpp
@@ -9,6 +9,8 @@
 #define FA_DEFAULT_HPP_
 
 #include <vector>
+#include <ctime>
+#include <string>
 
 namespace fa_default
 {
@@ -34,21 +36,104 @@ namespace fa_default
 
 		FaParam(const int, const int, const int, const double, const double, const double,
 				 const double, const double, const double, const double)
-		:T_Max(10000), Particles(100), Dimensions(100), Randomize(0.5), Attract(1.0), Absorb(0.5),
-		 Ran1_Max(1.5), Ran1_Min(-1.5), Ran2_Max(1.5), Ran2_Min(-1.5)
+		:t_max(10000), particles(100), dimensions(100), randomize(0.5), attract(1.0), absorb(0.5),
+		 ran1_Max(1.5), ran1_Min(-1.5), ran2_Max(1.5), ran2_Min(-1.5)
 		{}
 		
 		/* パラメータの定義 */
-    	int T_Max;        // 更新回数
-    	int Particles;    // 個体数
-    	int Dimensions;   // 次元数
-    	double Randomize; // ランダム性パラメータα
-    	double Attract;   // 発光地点における誘引度β
-    	double Absorb;    // 吸収係数γ
-    	double Ran1_Max;  // 初期位置のランダム生成(最大値)
-    	double Ran1_Min;  // 初期位置のランダム生成(最小値)
-    	double Ran2_Max;  // ランダム移動の範囲(最大値)
-    	double Ran2_Min;  // ランダム移動の範囲(最小値)
+    	int t_max;        // 更新回数
+    	int particles;    // 個体数
+    	int dimensions;   // 次元数
+    	double randomize; // ランダム性パラメータα
+    	double attract;   // 発光地点における誘引度β
+    	double absorb;    // 吸収係数γ
+    	double ran1_Max;  // 初期位置のランダム生成(最大値)
+    	double ran1_Min;  // 初期位置のランダム生成(最小値)
+    	double ran2_Max;  // ランダム移動の範囲(最大値)
+    	double ran2_Min;  // ランダム移動の範囲(最小値)
+	};
+
+	struct FaStrat
+	{
+	protected:
+
+    	double value;                               	   // 最良評価値
+    	std::vector<double> update_value;				   // 各更新時の最良評価値
+    	std::vector<double> variable;                      // 最良評価値の時の変数
+    	std::vector<double> init_variable;                 // 最適化開始時の最良評価値の変数
+    	std::vector<std::vector<double>> update_variable;
+    	double evals;                                      // 評価回数
+    	clock_t start_time;                                // 1試行あたりの最適化の実行時間
+    	size_t iter;                                       // 更新回数
+
+	public:
+
+    	FaStrat(){}
+
+		FaStrat& set_value(double input)
+		{
+			value = input;
+			return *this; 
+		}
+
+		FaStrat& set_update_value(std::vector<double> input)
+		{ 
+			update_value = input;
+			return *this; 
+		}
+
+		FaStrat& set_variable(std::vector<double> input)
+		{ 
+			variable = input;
+			return *this; 
+		}  
+
+		FaStrat& set_init_variable(std::vector<double> input)
+		{ 
+			init_variable = input;
+			return *this; 
+		}  
+
+		FaStrat& set_update_variable(std::vector<std::vector<double>> input)
+		{ 
+			update_variable = input;
+			return *this; 
+		}
+
+		FaStrat& set_evals(double input)
+		{ 
+			evals = input;
+			return *this; 
+		}
+
+		FaStrat& set_iter(size_t input)
+		{ 
+			iter = input;
+			return *this; 
+		}
+
+		FaStrat& set_time(clock_t input)
+		{ 
+			start_time = input;
+			return *this; 
+		}
+
+		double get_value() const
+		{ return value; }
+		std::vector<double> get_update_value() const
+		{ return update_value; }
+		std::vector<double> get_variable() const
+		{ return variable; }
+		std::vector<double> get_init_variable() const
+		{ return init_variable; }
+		std::vector<std::vector<double>> get_update_variable() const
+		{ return update_variable; }   
+		double get_evals() const
+		{ return evals; }
+		size_t get_iter() const
+		{ return iter; }
+		clock_t get_time() const
+		{ return start_time; }
 	};
 }
 
