d21143b2575bcb62fc0f5ae066ff07452fd27be6
9071-dynamix <88184928+9071-dynamix@users.noreply.github.com>
2021-11-26 19:28:44 +0900

    create ``get function & set function``
    ・パラメータ名の変更
    ・get関数、set関数の作成
    ・FaResultの作成

--- lib/fa/fa_default.hpp
+++ lib/fa/fa_default.hpp
@@ -15,6 +15,13 @@
 
 namespace fa_default
 {
+	struct FaOptimizer
+	{
+		/*
+		この中にoptimizeという関数
+		・initialize(初期化)
+		*/
+	};
 	
 	struct FireFly
     {
@@ -40,6 +47,117 @@ namespace fa_default
 	{
 	protected:
 
+    	/* パラメータの定義 */
+    	size_t t_max;        // 更新回数
+    	size_t particles;    // 個体数
+    	size_t dimensions;   // 次元数
+    	double randomize; // ランダム性パラメータα
+    	double attract;   // 発光地点における誘引度β
+    	double absorb;    // 吸収係数γ
+    	double init_pos_max;  // 初期位置のランダム生成(最大値)
+    	double init_pos_min;  // 初期位置のランダム生成(最小値)
+    	double ran_fly_max;  // ランダム移動の範囲(最大値)
+    	double ran_fly_min;  // ランダム移動の範囲(最小値)
+	
+	public:
+
+		FaParam(const int, const int, const int, const double, const double, const double,
+				const double, const double, const double, const double)
+		:t_max(10000), particles(100), dimensions(10), randomize(0.5), attract(1.0), absorb(0.5),
+		 init_pos_max(1.5), init_pos_min(-1.5), ran_fly_max(0.5), ran_fly_min(-0.5)
+		{}
+
+		int get_t_max() const
+    	{ return t_max; }
+    	int get_particles() const
+    	{ return particles; }
+		double get_dimensions() const
+		{ return dimensions; }
+		double get_randomize() const
+		{ return randomize; }
+		double get_attract() const
+		{ return attract; }
+		double get_absorb() const
+		{ return absorb; }
+		double get_init_pos_max() const
+		{ return init_pos_max; }
+		double get_init_pos_min() const
+		{ return init_pos_min; }
+		double get_ran_fly_max() const
+		{ return ran_fly_max; }
+		double get_ran_fly_min() const
+		{ return ran_fly_min; }
+
+		FaParam& set_t_max(const size_t input)
+		{
+			t_max = input;
+			return *this;
+		}
+
+		FaParam& set_particles(const size_t input)
+		{
+			particles = input;
+			return *this;
+		}
+
+		FaParam& set_dimensions(const size_t input)
+		{
+			dimensions = input;
+			return *this;
+		}
+
+		FaParam& set_randomize(const double input)
+		{
+			randomize = input;
+			return *this;
+		}
+
+		FaParam& set_attract(const double input)
+		{
+			attract = input;
+			return *this;
+		}
+
+		FaParam& set_absorb(const double input)
+		{
+			absorb = input;
+			return *this;
+		}
+
+		FaParam& set_init_pos_max(const double input)
+		{
+			init_pos_max = input;
+			return *this;
+		}
+
+		FaParam& set_init_pos_min(const double input)
+		{
+			init_pos_min = input;
+			return *this;
+		}
+
+		FaParam& set_ran_fly_max(const double input)
+		{
+			ran_fly_max = input;
+			return *this;
+		}
+
+		FaParam& set_ran_fly_min(const double input)
+		{
+			ran_fly_min = input;
+			return *this;
+		}
+	};
+
+	struct FaStrat
+	{
+		
+	};
+
+	struct FaResult
+	{
+	protected:
+
     	double value;                               	   // 最良評価値
     	std::vector<double> update_value;				   // 各更新時の最良評価値
     	std::vector<double> variable;                      // 最良評価値の時の変数
@@ -47,73 +165,53 @@ namespace fa_default
     	std::vector<std::vector<double>> update_variable;
     	double evals;                                      // 評価回数
     	clock_t start_time;                                // 1試行あたりの最適化の実行時間
-    	size_t iter;                                       // 更新回数
-	
+    	size_t iter;
+
 	public:
 
-		FaParam(const int, const int, const int, const double, const double, const double,
-				const double, const double, const double, const double)
-		:t_max(10000), particles(100), dimensions(10), randomize(0.5), attract(1.0), absorb(0.5),
-		 ran1_max(1.5), ran1_min(-1.5), ran2_max(1.5), ran2_min(-1.5)
-		{}
-		
-		/* パラメータの定義 */
-    	int t_max;        // 更新回数
-    	int particles;    // 個体数
-    	int dimensions;   // 次元数
-    	double randomize; // ランダム性パラメータα
-    	double attract;   // 発光地点における誘引度β
-    	double absorb;    // 吸収係数γ
-    	double ran1_max;  // 初期位置のランダム生成(最大値)
-    	double ran1_min;  // 初期位置のランダム生成(最小値)
-    	double ran2_max;  // ランダム移動の範囲(最大値)
-    	double ran2_min;  // ランダム移動の範囲(最小値)
-
-		FaParam(){}	
-
-		FaParam& set_value(double input)
+		FaResult& set_value(double input)
 		{
 			value = input;
 			return *this; 
 		}
 
-		FaParam& set_update_value(std::vector<double> input)
+		FaResult& set_update_value(std::vector<double> input)
 		{ 
 			update_value = input;
 			return *this; 
 		}
 
-		FaParam& set_variable(std::vector<double> input)
+		FaResult& set_variable(std::vector<double> input)
 		{ 
 			variable = input;
 			return *this; 
 		}  
 
-		FaParam& set_init_variable(std::vector<double> input)
+		FaResult& set_init_variable(std::vector<double> input)
 		{ 
 			init_variable = input;
 			return *this; 
 		}  
 
-		FaParam& set_update_variable(std::vector<std::vector<double>> input)
+		FaResult& set_update_variable(std::vector<std::vector<double>> input)
 		{ 
 			update_variable = input;
 			return *this; 
 		}
 
-		FaParam& set_evals(double input)
+		FaResult& set_evals(double input)
 		{ 
 			evals = input;
 			return *this; 
 		}
 
-		FaParam& set_iter(size_t input)
+		FaResult& set_iter(size_t input)
 		{ 
 			iter = input;
 			return *this; 
 		}
 
-		FaParam& set_time(clock_t input)
+		FaResult& set_time(clock_t input)
 		{ 
 			start_time = input;
 			return *this; 
